ecc6d58bac98468524f9d498ac8088bdc3bd0d82
KetchuppOfficial <dolgov04@list.ru>
2026-05-10 22:59:56 +0300

    [12-02] done

---
+++ 12-02-quine/src/main.cpp
@@ -0,0 +1,5 @@
+#include <cstdio>
+
+const char* s = "#include <cstdio>%c%cconst char* s = %c%s%c;%c%cint main() { std::printf(s, 10, 10, 34, s, 34, 10, 10, 10); }%c";
+
+int main() { std::printf(s, 10, 10, 34, s, 34, 10, 10, 10); }
